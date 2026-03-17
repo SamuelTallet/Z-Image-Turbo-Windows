2e39d3dc6bd744e854e755720596a5d19693a91c
Samuel Tallet <samuel.tallet@gmail.com>
2026-03-17 15:18:42 +0100

    Change read method

--- source/py/image_models.py
+++ source/py/image_models.py
@@ -9,7 +9,6 @@ def get_image_models(json_file: Path) -> list[ImageModel]:
     """Get image models from a JSON file."""
     adapter = TypeAdapter(list[ImageModel])
 
-    with open(json_file, "r") as file:
-        models_json = file.read()
+    models_json = json_file.read_text(encoding="utf-8")
 
     return adapter.validate_json(models_json)
